c2a8ad928b63e6edad8dddcccbd508dacd9538ad
val <valLoukach@gmail.com>
2020-10-08 15:34:19 -0400

    commit t2

--- SC/SC/ContentView.swift
+++ SC/SC/ContentView.swift
@@ -1,10 +1,12 @@
 //comm
+//comm
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+          .padding()
+          .padding()
     }
 }
 
