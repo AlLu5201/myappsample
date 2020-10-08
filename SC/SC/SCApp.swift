cd080d450d826ec43dd6fc7c129a54068f4ea878
val <valLoukach@gmail.com>
2020-10-08 15:28:26 -0400

    ommit sc

---
+++ SC/SC/SCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCApp.swift
+//  SC
+//
+//  Created by val on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct SCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
