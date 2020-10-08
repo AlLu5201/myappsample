d1cee1b7e22946423aeb31cd632b70df066cdc04
val <valLoukach@gmail.com>
2020-10-08 12:00:34 -0400

    Initial Commit

---
+++ SocialCapitalT/SocialCapitalTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialCapitalTApp.swift
+//  SocialCapitalT
+//
+//  Created by val on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct SocialCapitalTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
